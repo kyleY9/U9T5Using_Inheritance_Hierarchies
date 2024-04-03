59172dd5abfcd13361b348562dd6628d63942cbd
Kyle Yang <KyleY9@nycstudents.net>
2024-04-02 21:22:49 -0400

    Last part was too hard, but rest I finished.

---
+++ src/GridGame.java
@@ -0,0 +1,183 @@
+import java.util.Scanner;
+
+public class GridGame {
+    private Space[][] board;
+    private Player player;
+    private Scanner scanner;
+
+    public GridGame() {
+        scanner = new Scanner(System.in);
+        createPlayer();
+        setupBoard();
+        play();
+    }
+
+    private void createPlayer() {
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+        player = new Player(name);
+    }
+
+    // initialize the board instance variable to be a 8x8 board;
+    // place new Space object with "_" as the symbol into each board position;
+    // place the Player object at lower left corner;
+    // initialize and place a Goal object with the symbol "X" in the upper right corner;
+    // create several Treasure objects (up to you how many), with symbol of your choice,
+    // each with a point value that you decide, and place them throughout the board
+    private void setupBoard() {
+        board = new Space[8][8];
+        board[0][7] = new Goal("X");
+        board[7][0] = player;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int k = 0; k < board[0].length; k++) {
+                if (board[i][k] == null) {
+                    if (randTreasureLocation()) {
+                        board[i][k] = new Treasure("$", randPointValue());
+                    } else {
+                        board[i][k] = new Space("_");
+                    }
+                }
+            }
+        }
+    }
+
+    // helper method: there's a 33% chance for treasure to spawn at a space
+    public boolean randTreasureLocation() {
+        int num = (int) (Math.random() * 3) + 1;
+        return num == 3;
+    }
+
+    // helper method: returns a random point value for a piece of treasure (1-50)
+    public int randPointValue() {
+        return (int) (Math.random() * 30) + 1;
+    }
+
+    /* prints the 2D array board, showing the symbol for each Space, e.g.
+       _______X
+       __#_____
+       _____#__
+       _#______
+       ________
+       ______#_
+       ________
+       M___#___
+     */
+    private void printBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int k = 0; k < board[0].length; k++) {
+                System.out.println(board[i][k].getSymbol());
+            }
+            System.out.println();
+        }
+    }
+
+    // plays the game;
+    private void play() {
+
+        // WRITE THIS METHOD
+        // main game loop:
+        // while the player has not yet reached the goal, print the board (complete can call helper method above)
+        // then asks user to enter a direction: W, A, S, D (up, left, down, right).
+        // if the intended direction is in bounds, move the Player to the new location and fill previous position
+        // with a Space object (with "_" symbol).
+        // if player moves to a position occupied by a Treasure, add its point value to the players score,
+        // and replace that element with a Space object (with "_" symbol).
+        // if the player reaches the goal, end the game and print their final score and the number of moves it took
+        while (!(board[0][7].equals(player.getSymbol()))) {
+            printBoard();
+            System.out.println("Enter either W, A, S, or D");
+            String response = scanner.nextLine();
+
+            if ((!response.equals("W")) && (!response.equals("A")) & (!response.equals("S")) && (!response.equals("D"))) {
+                System.out.println("Invalid Response! W, A, S, or D!");
+            } else {
+                
+            }
+
+
+        }
+    }
+
+    // more helper methods
+    public void move(String response) {
+        if (response.equals("W")) {
+            moveUp();
+        } else if (response.equals("A")) {
+            moveLeft();
+        } else if (response.equals("S")) {
+            moveDown();
+        } else {
+            moveRight();
+        }
+    }
+
+    public int findPlayerRow() {
+        for (int i = 0; i < board.length; i++) {
+            for (int k = 0; k < board[0].length; k++) {
+                if (board[i][k].equals(player.getSymbol())) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int findPlayerColumn() {
+        for (int i = 0; i < board.length; i++) {
+            for (int k = 0; k < board[0].length; k++) {
+                if (board[i][k].equals(player.getSymbol())) {
+                    return k;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public void moveLeft() {
+        int row = findPlayerRow();
+        int col = findPlayerColumn();
+        if (col != 0) {
+            if (board[row][col - 1].getSymbol().equals("$")) {
+                /// ???? this is hard;
+            }
+            board[row][col - 1] = player;
+            board[row][col] = new Space("_");
+        } else {
+            System.out.println("You can't do that!");
+        }
+    }
+
+    public void moveUp() {
+        int row = findPlayerRow();
+        int col = findPlayerColumn();
+        if (row != 0) {
+            board[row + 1][col] = player;
+            board[row][col] = new Space("_");
+        } else {
+            System.out.println("You can't do that!");
+        }
+    }
+
+    public void moveDown() {
+        int row = findPlayerRow();
+        int col = findPlayerColumn();
+        if (col != 7) {
+            board[row - 1][col] = player;
+            board[row][col] = new Space("_");
+        } else {
+            System.out.println("You can't do that!");
+        }
+    }
+
+    public void moveRight() {
+        int row = findPlayerRow();
+        int col = findPlayerColumn();
+        if (col != 7) {
+            board[row][col + 1] = player;
+            board[row][col] = new Space("_");
+        } else {
+            System.out.println("You can't do that!");
+        }
+    }
+}
---
+++ src/GridRunner.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class GridRunner {
+    public static void main(String[] args) {
+
+    }
+}
